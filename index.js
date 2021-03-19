7b2467c26fc2ba790995bdd5bf4632b8de37c93f
jycdjin <deathjinderekn2@gmail.com>
2021-03-18 22:36:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
+for(const file of commandsFiles){
     const command = require(`./commands/${file}`);
     
     client.commands.set(command.name, command);
